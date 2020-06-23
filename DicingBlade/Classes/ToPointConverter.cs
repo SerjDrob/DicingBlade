43544753f554fcbdee54c86abf8c2cbbd0429ad0
Сергей Дробышев <serjdrob@gmail.com>
2020-06-23 17:34:21 +0700

    23.06.20

---
+++ DicingBlade/Classes/ToPointConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+namespace DicingBlade.Classes
+{
+    class ToPointConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new System.Windows.Point(System.Convert.ToDouble(values[0]), System.Convert.ToDouble(values[1]));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
