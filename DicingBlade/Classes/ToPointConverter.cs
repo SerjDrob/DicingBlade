9ef4e4e8bfd7b6e3ef3275b3581d768a4f1c55f7
Сергей Дробышев <serjdrob@gmail.com>
2020-06-24 15:44:37 +0300

    24.06.20

--- DicingBlade/Classes/ToPointConverter.cs
+++ DicingBlade/Classes/ToPointConverter.cs
@@ -14,7 +14,17 @@ namespace DicingBlade.Classes
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return new System.Windows.Point(System.Convert.ToDouble(values[0]), System.Convert.ToDouble(values[1]));
+            double x = 0;
+            double y = 0;
+            try
+            {
+                x = System.Convert.ToDouble(values[0]);
+                y = System.Convert.ToDouble(values[1]);                
+            }
+            catch
+            {                
+            }
+            return new System.Windows.Point(x, y);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
