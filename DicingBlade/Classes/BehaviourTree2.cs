e5560709a783444d38c0696ee1f2ba563c616000
Сергей Дробышев <serjdrob@gmail.com>
2021-06-22 13:24:11 +0700

    Work on BT2

--- DicingBlade/Classes/BehaviourTree2.cs
+++ DicingBlade/Classes/BehaviourTree2.cs
@@ -20,15 +20,26 @@ namespace DicingBlade.Classes
         {            
             private Func<Task> _myAction;
            
-            Leaf(Func<Task> myWork)
+            public Leaf(Func<Task> myWork)
             {
                 _myAction = myWork;
             }
             public override async Task<bool> DoWork()
             {
-                base.DoWork();
-                await _myAction();
-                return true;
+                if (!_block.IsBlocked)
+                {
+                    await base.DoWork();
+                    try
+                    {
+                        await _myAction();
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                    return true; 
+                }
+                return false;
             }
 
             public override void SetPauseToken(PauseTokenSource pauseTokenSource)
@@ -43,6 +54,11 @@ namespace DicingBlade.Classes
             {
                 _workers = new List<Worker>();  
             }
+            public override Sequence SetBlock(Block block)
+            {
+                base.SetBlock(block);
+                return this;
+            }
             public Sequence Hire(Worker worker)
             {
                 _workers.Add(worker);
@@ -50,15 +66,20 @@ namespace DicingBlade.Classes
             }
             public override async Task<bool> DoWork()
             {
-                try
+                if (!_block.IsBlocked)
                 {
-                    _workers?.ForEach(worker => worker.DoWork());
+                    await base.DoWork();
+                    try
+                    {
+                        _workers?.ForEach(worker => worker.DoWork());
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                    return true; 
                 }
-                catch (Exception)
-                {
-                    throw;
-                }
-                return true;
+                return false;
             }
 
             public override void SetPauseToken(PauseTokenSource pauseTokenSource)
@@ -66,16 +87,71 @@ namespace DicingBlade.Classes
                 _workers.ForEach(worker => worker.SetPauseToken(pauseTokenSource));
             }
         }
-        public abstract class Worker
-        {            
-            public virtual async Task<bool> DoWork()
+        public class Ticker : Worker
+        {
+            private Worker _worker;
+            public Ticker Hire(Worker worker)
             {
-                await _pauseTokenSource?.Token.WaitWhilePausedAsync();
-                //KnowMyName?.Invoke(_myName);    
+                _worker = worker;
+                return this;
+            }
+            public override Ticker SetBlock(Block block)
+            {
+                base.SetBlock(block);
+                return this;
+            }
+            public override async Task<bool> DoWork()
+            {
+                await base.DoWork();
+                while (!_block.IsBlocked)
+                {
+                    try
+                    {
+                        await _worker.DoWork();
+                    }
+                    catch (Exception)
+                    {
+                        return false;                      
+                    }
+                }
                 return true;
             }
+            public override void SetPauseToken(PauseTokenSource pauseTokenSource)
+            {
+                _pauseTokenSource = pauseTokenSource;
+            }
+        }
+        public abstract class Worker
+        {           
+            public virtual Worker SetBlock(Block block)
+            {
+                _block = block;
+                return this;
+            }
+            public virtual async Task<bool> DoWork()
+            {
+                await _pauseTokenSource?.Token.WaitWhilePausedAsync();                    
+                return true;
+            }
+            protected Block _block;
             protected PauseTokenSource _pauseTokenSource;
             public abstract void SetPauseToken(PauseTokenSource pauseTokenSource);
         }
+        public class Block
+        {
+            public bool IsBlocked { get; set; } = false;
+        }
+
+
+        public class Test
+        {
+            Test()
+            {
+                var l = new Leaf(async ()=> { });
+                               
+
+            }
+        }
+
     }
 }
