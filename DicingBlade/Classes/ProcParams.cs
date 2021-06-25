f08ed9c28fa371585ae4398ba2287210bdae6b5a
Сергей Дробышев <serjdrob@gmail.com>
2021-06-25 18:24:19 +0700

    Flow indicator added

---
+++ DicingBlade/Classes/ProcParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DicingBlade
+{
+    public struct ProcParams
+    {
+        public double currentShift;
+        public double currentSideAngle;
+    }
+}
---
+++ DicingBlade/UserControls/HeightToRadiusConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DicingBlade.UserControls
+{
+    public class HeightToRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double height = (double)value;
+            return height / 4;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new NotImplementedException();
+        }
+    }
+}
